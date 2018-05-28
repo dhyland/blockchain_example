bcd98fbc42d9f4a1d28e9cfe56a6869a8d432666
David <dhyland17@gmail.com>
2018-05-28 05:24:57 +0100

    Author: David Hyland Description: p2p network start. Need to test

---
+++ src/blockchain_example/network/NetworkUtils.java
@@ -0,0 +1,10 @@
+package blockchain_example.network;
+
+//this class will be for network utilities.... to be continued
+public abstract class NetworkUtils {
+
+	//TODO
+	public static String execute(String[] args) {
+		return null;
+	}
+}
--- src/blockchain_example/network/P2P.java
+++ src/blockchain_example/network/P2P.java
@@ -1,5 +1,95 @@
-package blockchain_example;
+package blockchain_example.network;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.*;
+import java.util.logging.Level;
+
+import blockchain_example.MyLogger;
+
+
+
+
+//class to implement basic peer-to-peer 
 public class P2P {
 
+	private int port;
+	private DataOutputStream outputStream;
+	private ArrayList<Peer> peerList;
+	public Thread serverThread;
+	private boolean serverIsOn;
+	private ServerSocket serverSocket;
+	private Socket socket = null;
+	
+	 //Node with access to blockchain
+    public P2P(int port0){
+        this.port = port0;
+        
+        peerList = new ArrayList<>();
+        serverThread = new Thread(new Runnable(){
+	        public void run(){
+	        	try{
+	        		listen();
+	        		MyLogger.log(Level.INFO, "Connection Ended");
+	        	}
+	        	catch (IOException e){
+	        	}
+	        }
+        });		
+        //initializeCommands();
+    }
+    
+    public void start(){
+        if(serverThread.isAlive()){
+            MyLogger.log(Level.INFO, "Server is already running.");
+            return;
+        }
+        serverIsOn = true;
+        serverThread.start();
+    }
+
+    public void stop() throws IOException{
+    		serverIsOn = false;
+    		try {
+        		serverThread.interrupt();
+    			socket.close();
+        } catch (NullPointerException n){
+        	MyLogger.log(Level.WARNING, "Null pointer when closing server socket");
+        }
+    		
+    	MyLogger.log(Level.INFO, "Server Stopped");
+    }
+
+    public void listen() throws IOException, SocketTimeoutException{
+    	serverSocket = new ServerSocket(this.port);
+		MyLogger.log(Level.INFO, "Starting server on port: " + this.port);
+			
+		Peer peer;
+		serverSocket.setSoTimeout(10000);
+        while(serverIsOn){
+			try{
+				socket = serverSocket.accept();
+				MyLogger.log(Level.INFO, "Passed Accept");
+				peer = new Peer(socket);
+				MyLogger.log(Level.INFO, "Connection received from: " + peer.toString());
+				peerList.add(peer);
+				MyLogger.log(Level.INFO, "New peer: " + peer.toString());
+			}
+			catch (SocketTimeoutException e){
+        			
+        	}
+        }
+    }
+
+    public void connect(Socket socket){
+		try{
+			outputStream = new DataOutputStream(socket.getOutputStream());
+			Peer.send("ping", outputStream);		
+		}
+        catch (IOException e){
+            
+        }
+    }
 }
